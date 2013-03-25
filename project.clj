be8ce352c87243e915cbb079b26e7f7c06bdcf73
Jeremy Hall <github@wallslide.com>
2013-03-25 18:10:29 +0900

    Initial Upload Interface

--- project.clj
+++ project.clj
@@ -8,6 +8,16 @@
                  [cheshire "5.0.2"]
                  [enfocus "1.0.1"]]
   :plugins [[lein-ring "0.8.2"]]
+  :cljsbuild {
+    :builds [{
+      ; The path to the top-level ClojureScript source directory:
+      :source-paths ["src-cljs"]
+      ; The standard ClojureScript compiler options:
+      ; (See the ClojureScript compiler documentation for details.)
+      :compiler {
+        :output-to "resources/js/main.js"
+        :optimizations :whitespace
+        :pretty-print true}}]}
   :ring {:handler my-example-website.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
